9a23b3dc1a013cf03bb2e18287136e85803ec745
dustin1818 <dustinamoda18@gmail.com>
2023-10-15 00:08:14 +0800

    Add SupplierList

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,40 +1,45 @@
 import SearchInput from "../components/SearchInput";
+import SupplierList from "./SupplierList";
 
 const Card = () => {
   return (
     <div className="card">
-      <h1 className="card_heading">Browse</h1>
-      <div className="card_heading_icon_container">
-        <button className="card_heading_icon_container_btn_x">
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="x">
-              <path
-                id="Vector"
-                d="M15 5L5 15"
-                stroke="#7B7B7B"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                id="Vector_2"
-                d="M5 5L15 15"
-                stroke="#7B7B7B"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </g>
-          </svg>
-        </button>
+      <div className="card_upper">
+        <h1 className="card_heading">Browse</h1>
+        <div className="card_heading_icon_container">
+          <button className="card_heading_icon_container_btn_x">
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="x">
+                <path
+                  id="Vector"
+                  d="M15 5L5 15"
+                  stroke="#7B7B7B"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  id="Vector_2"
+                  d="M5 5L15 15"
+                  stroke="#7B7B7B"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </g>
+            </svg>
+          </button>
+        </div>
+        <SearchInput />
       </div>
-      <SearchInput />
+      <hr style={{ marginTop: "24px" }} />
+      <SupplierList />
     </div>
   );
 };
---
+++ src/components/SupplierList.jsx
@@ -0,0 +1,31 @@
+const SupplierList = () => {
+  return (
+    <div className="supplier_list_container">
+      <div className="supplier_list_container_name">
+        <div className="supplier_list_container_name_wrapper">
+          <div className="supplier_list_container_name_wrapper_inner">
+            <p>AC&C</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+            >
+              <path
+                d="M5.625 11.25L9.375 7.5L5.625 3.75"
+                stroke="#C4C4C4"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SupplierList;
