f1f985fb1701fdc92a3136bf85432ef4c39f49a6
dustin1818 <dustinamoda18@gmail.com>
2023-10-22 20:18:13 +0800

    update select product, bug not yet fixed

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { FormContext } from "../context/FormContext";
 import Img from "../assets/image.svg";
 import downArrow from "../assets/chevron-down.png";
@@ -15,28 +15,51 @@ const ProductList = () => {
     id: 0,
     display: false,
   });
-  console.log(
-    Array.isArray(productsList) ? productsList.map((prod) => prod) : null
-  );
+
+  const [childProducts, setChildProducts] = useState([]);
+  const [inputValue, setInputValue] = useState(1);
+  const [isChecked, setIsChecked] = useState(false);
+  const [subProdID, setSubProdID] = useState(0);
+  const [productListSelected, setProductListSelected] = useState([]);
+
   const getProduct = (id) => {
     setIsDisabled((prevState) => (prevState = false));
-    productsList.map((products) =>
-      products.id === id
-        ? setSelected({
-            id: products.id,
-            display: !selected.display,
-          })
-        : null
-    );
+    productsList.map((products) => {
+      if (products.id === id) {
+        setSelected({ id: id, display: true });
+        setChildProducts((prevState) => (prevState = products.childProducts));
+        // setIsChecked(false);
+      }
+    });
   };
 
+  const selectInput = (prod) => {
+    setIsChecked((prevState) => (prevState = true));
+    childProducts.map((products) => {
+      if (prod.id === products.id) {
+        setSubProdID((prevState) => (prevState = prod.id));
+        setProductQuantity((prevState) => (prevState = prod.quantity));
+        setProductListSelected((prevState) => (prevState = prod));
+        console.log(productListSelected);
+      }
+    });
+  };
+
+  // const changeInput = (prod) => {
+  //   console.log("changed");
+  //   // if()
+  // };
+
+  // console.log(childProducts);
   return (
     <div className="product_list_container">
       <div className="product_list_container_name">
         {Array.isArray(productsList)
           ? productsList.map((prod) => (
               <div
-                className="product_list_container_name_wrapper"
+                className={`product_list_container_name_wrapper ${
+                  selected.id === prod.id ? "selected" : ""
+                }`}
                 key={prod.id}
                 onClick={() => getProduct(prod.id)}
               >
@@ -70,9 +93,69 @@ const ProductList = () => {
                 </div>
                 <hr style={{ border: "solid 1px #e5e5e5" }} />
                 <div>
-                  {selected.id === prod.id && selected.display === true ? (
-                    <p>hello</p>
-                  ) : null}
+                  {selected.id === prod.id && selected.display === true
+                    ? productsList.map((products) =>
+                        prod.id === products.id
+                          ? products.childProducts.map((prod) => (
+                              <>
+                                <p>
+                                  isChecked:{" "}
+                                  {isChecked && subProdID === prod.id
+                                    ? true.toString()
+                                    : false.toString()}
+                                </p>
+                                <div
+                                  className="product_list_container_name_wrapper_inner"
+                                  key={prod.id}
+                                >
+                                  <div className="product_list_container_name_wrapper_inner_div2">
+                                    <input
+                                      type="checkbox"
+                                      checked={
+                                        isChecked && subProdID === prod.id
+                                          ? true
+                                          : false
+                                      }
+                                    />
+                                    <div className="product_list_container_name_wrapper_inner_div1_holder">
+                                      <p
+                                        className="product-text"
+                                        style={{
+                                          textTransform: "capitalize",
+                                        }}
+                                      >
+                                        {prod.name}
+                                      </p>
+                                      <p className="product-subtext">
+                                        {" "}
+                                        SKU: {prod.sku}
+                                      </p>
+                                    </div>
+                                  </div>
+                                  {isChecked ? (
+                                    <input
+                                      className="product-number-input active"
+                                      type="number"
+                                      value={inputValue}
+                                      // onChange={() => changeInput(prod)}
+                                      onClick={() => selectInput(prod)}
+                                    />
+                                  ) : (
+                                    <input
+                                      className="product-number-input"
+                                      type="number"
+                                      value={inputValue}
+                                      // onChange={() => changeInput(prod)}
+                                      onClick={() => selectInput(prod)}
+                                    />
+                                  )}
+                                </div>
+                                <hr style={{ border: "solid 1px #e5e5e5" }} />{" "}
+                              </>
+                            ))
+                          : null
+                      )
+                    : null}
                 </div>
               </div>
             ))
